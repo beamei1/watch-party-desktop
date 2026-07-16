dd816e53868ad6f13c2f266f198d2ea4040d1df6
beamei1 <72058892+beamei1@users.noreply.github.com>
2026-07-16 15:07:43 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, session } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    title: "Watch Party",
+    autoHideMenuBar: true, 
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // อนุญาตไมค์อัตโนมัติ
+  session.defaultSession.setPermissionRequestHandler((webContents, permission, callback) => {
+    if (permission === 'media') {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+
+  win.loadURL('https://bpdow.vercel.app');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
